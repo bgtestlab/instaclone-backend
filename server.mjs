8a955b1b1a9dcf6df14e8185b4509b15148ce7c8
codeflow <boramismn@gmail.com>
2023-10-03 22:44:16 +0900

    4.19X apply express server

--- server.mjs
+++ server.mjs
@@ -2,21 +2,65 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+//import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { typeDefs, resolvers } from "./schema.mjs";
 import { getUser } from "./users/users.utils.mjs";
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
-  },
-});
-
 const PORT = process.env.PORT;
 
-const { url } = await startStandaloneServer(server, { listen: { port: PORT } });
-console.log(`🚀 Server ready at ${url}`);
+// const server = new ApolloServer({
+//   resolvers,
+//   typeDefs,
+// context: async ({ req }) => {
+//   return {
+//     loggedInUser: await getUser(req.headers.token),
+//   };
+//   },
+// });
+
+// Required logic for integrating with Express
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+// Ensure we wait for our server to start
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  "/",
+  cors(),
+  bodyParser.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    //context: async ({ req }) => ({ token: req.headers.token }),
+    context: async ({ req }) => {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    },
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+
+//const { url } = await startStandaloneServer(server, { listen: { port: PORT } });
+console.log(`🚀 Server ready at http://localhost:4000/`);
