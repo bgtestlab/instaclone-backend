b657cfe5247ce1913e2da0dd551665b67e507c1c
codeflow <boramismn@gmail.com>
2023-10-09 20:59:58 +0900

    6.3 Upload Photo part Two

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,13 +1,29 @@
+import client from "../../client.mjs";
+
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
         if (caption) {
           // parse caption
+          const hashtags = caption.match(/#[\w]+/g);
+
           // get or create hashtags
         }
         // save the photo w/ the parsed hashtags
         // add the photo to the hashtags
+        client.photo.create({
+          data: {
+            file,
+            caption,
+            hashtags: {
+              connectOrCreate: [
+                { where: { hashtag: "#food" } },
+                { create: { hashtag: "food" } },
+              ],
+            },
+          },
+        });
       }
     ),
   },
