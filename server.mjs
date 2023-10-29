b8f06a98686deaf8dc29819795b2d06711572758
codeflow <boramismn@gmail.com>
2023-10-29 11:42:22 +0900

    7.11 Authenticating Subscriptions

--- server.mjs
+++ server.mjs
@@ -77,12 +77,30 @@ app.use(
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => {
-      if (req) {
+    context: async (ctx) => {
+      if (ctx.req) {
         return {
-          loggedInUser: await getUser(req.headers.token),
+          loggedInUser: await getUser(ctx.req.headers.token),
+        };
+      } else {
+        const {
+          connection: { context },
+        } = ctx;
+        return {
+          loggedInUser: context.loggedInUser,
         };
       }
+    },
+    subscriptions: {
+      onConnect: async ({ token }) => {
+        if (!token) {
+          throw new Error("You can't listen.");
+        }
+        const loggedInUser = await getUser(token);
+        return {
+          loggedInUser,
+        };
+      },
     },
   })
 );
